a957b8b4dcc974e1faf3d9494413f045087228c8
Tom <tomdion35@gmail.com>
2025-01-04 15:56:53 -0500

    fixed pathing issue in login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/register/', CreateUserView.as_view(), name = 'register'),
-    path('api/token/', TokenObtainPairView.as_view(), name = 'get_token'),
+    path('api/user/login/', TokenObtainPairView.as_view(), name = 'login'),
     path('api/token/refresh', TokenRefreshView.as_view(), name = 'refresh'),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls'))
