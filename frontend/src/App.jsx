ee20eb8a527a94eafc577f64db516ac1c61e02f8
Tom <tomdion35@gmail.com>
2025-01-04 15:09:13 -0500

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
-import react from 'react'
+import react from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/register';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to='login'/>
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-   
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="*" element={<NotFound />}> </Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
