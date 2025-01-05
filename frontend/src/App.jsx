f5c5a92a3d956febffa82bc455fae4ca64afd39d
Tom <tomdion35@gmail.com>
2025-01-05 15:44:49 -0500

    formatted app, home, and form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,42 @@
-import react from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './pages/login';
-import Register from './pages/register';
-import NotFound from './pages/NotFound';
-import Home from './pages/Home';
-import ProtectedRoute from './components/ProtectedRoute';
-
+import react from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function Logout() {
-  localStorage.clear()
-  return <Navigate to='login'/>
+  localStorage.clear();
+  return <Navigate to="login" />;
 }
 
 function RegisterAndLogout() {
-  localStorage.clear()
-  return <Register />
+  localStorage.clear();
+  return <Register />;
 }
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<RegisterAndLogout />} />
-          <Route path="*" element={<NotFound />}> </Route>
-        </Routes>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}>
+          {" "}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,86 +2,88 @@ import { useState, useEffect } from "react";
 import api from "../api";
 
 function Home() {
-    const [recipes, setRecipes] = useState([]);
-    const [url, setUrls] = useState("");
-    const [name, setName] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [url, setUrls] = useState("");
+  const [name, setName] = useState("");
 
-    useEffect(() => {
-        getRecipes();
-    }, [])
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
-    const getRecipes = async () => {
-        try {
-            const res = await api.get("/api/recipes/");
-            const data = res.data;
-            setRecipes(data);
-            console.log(data);
-        } catch (err) {
-            alert(err);
-        }
-    };
+  const getRecipes = async () => {
+    try {
+      const res = await api.get("/api/recipes/");
+      const data = res.data;
+      setRecipes(data);
+      console.log(data);
+    } catch (err) {
+      alert(err);
+    }
+  };
 
-    const deleteRecipe = async (id) => {
-        try {
-            const res = await api.delete(`/api/recipes/delete/${id}/`);
-            if (res.status === 204) {
-                alert("Recipe deleted");
-            } else {
-                alert("Failed to delete recipe.");
-            }
-        } catch (err) {
-            alert(err);
-        }
-        
-        await getRecipes();
-    };
+  const deleteRecipe = async (id) => {
+    try {
+      const res = await api.delete(`/api/recipes/delete/${id}/`);
+      if (res.status === 204) {
+        alert("Recipe deleted");
+      } else {
+        alert("Failed to delete recipe.");
+      }
+    } catch (err) {
+      alert(err);
+    }
 
-    const createRecipe = async (e) => {
-        e.preventDefault();
-        try {
-            console.log({ name, url })
-            const res = await api.post("/api/recipes/", { name, url });
-            if (res.status === 201) {
-                alert("Recipe added");
-            } else {
-                alert("Failed to create recipe.");
-            }
-        } catch (err) {
-            alert(err);
-        }
+    await getRecipes();
+  };
 
-        await getRecipes();
-    };
+  const createRecipe = async (e) => {
+    e.preventDefault();
+    try {
+      console.log({ name, url });
+      const res = await api.post("/api/recipes/", { name, url });
+      if (res.status === 201) {
+        alert("Recipe added");
+      } else {
+        alert("Failed to create recipe.");
+      }
+    } catch (err) {
+      alert(err);
+    }
 
-    return (
+    await getRecipes();
+  };
+
+  return (
     <div>
-        <div>
-            <h2>Recipes</h2>
-        </div>
-        <h2>Add a Recipe</h2>
-        <form onSubmit={createRecipe}>
-            <label htmlFor="name">Name:</label>
-        <br/>
-        <input 
-            type="text"
-            id="name"
-            name="name"
-            required onChange={(e) => setName(e.target.value)}
-            value={name}
-            />
-            <label htmlFor="url">Recipe URLs:</label>
-        <br/>
-        <textarea 
-            id="url"
-            name="url"
-            required value={url}
-            onChange={(e) => setUrls(e.target.value)}
-            ></textarea>
-            <br/>
-            <input type="submit" value ="Submit"></input>
-        </form>
+      <div>
+        <h2>Recipes</h2>
+      </div>
+      <h2>Add a Recipe</h2>
+      <form onSubmit={createRecipe}>
+        <label htmlFor="name">Name:</label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          required
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <label htmlFor="url">Recipe URLs:</label>
+        <br />
+        <textarea
+          id="url"
+          name="url"
+          required
+          value={url}
+          onChange={(e) => setUrls(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit"></input>
+      </form>
     </div>
-    )
+  );
 }
 
-export default Home
+export default Home;
