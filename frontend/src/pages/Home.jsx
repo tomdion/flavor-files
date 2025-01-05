22e0db789611caa0ae4b1762cfc7d753959924a7
Tom <tomdion35@gmail.com>
2025-01-05 13:08:57 -0500

    add create recipe, delete recipe, and get recipes to Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,87 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+
 function Home() {
-    return <div>Home</div>
+    const [recipes, setRecipes] = useState([]);
+    const [url, setUrls] = useState("");
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        getRecipes();
+    }, [])
+
+    const getRecipes = async () => {
+        try {
+            const res = await api.get("/api/recipes/");
+            const data = res.data;
+            setRecipes(data);
+            console.log(data);
+        } catch (err) {
+            alert(err);
+        }
+    };
+
+    const deleteRecipe = async (id) => {
+        try {
+            const res = await api.delete(`/api/recipes/delete/${id}/`);
+            if (res.status === 204) {
+                alert("Recipe deleted");
+            } else {
+                alert("Failed to delete recipe.");
+            }
+        } catch (err) {
+            alert(err);
+        }
+        
+        await getRecipes();
+    };
+
+    const createRecipe = async (e) => {
+        e.preventDefault();
+        try {
+            console.log({ name, url })
+            const res = await api.post("/api/recipes/", { name, url });
+            if (res.status === 201) {
+                alert("Recipe added");
+            } else {
+                alert("Failed to create recipe.");
+            }
+        } catch (err) {
+            alert(err);
+        }
+
+        await getRecipes();
+    };
+
+    return (
+    <div>
+        <div>
+            <h2>Recipes</h2>
+        </div>
+        <h2>Add a Recipe</h2>
+        <form onSubmit={createRecipe}>
+            <label htmlFor="name">Name:</label>
+        <br/>
+        <input 
+            type="text"
+            id="name"
+            name="name"
+            required onChange={(e) => setName(e.target.value)}
+            value={name}
+            />
+            <label htmlFor="url">Recipe URLs:</label>
+        <br/>
+        <textarea 
+            id="url"
+            name="url"
+            required value={url}
+            onChange={(e) => setUrls(e.target.value)}
+            ></textarea>
+            <br/>
+            <input type="submit" value ="Submit"></input>
+        </form>
+    </div>
+    )
 }
 
 export default Home
